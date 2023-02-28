069e73138345f7187c42a36797d1e062b931c392
ngohongthong <baphongpine@gmail.com>
2023-02-28 22:37:06 +0700

    add and config handlebars config static and scss with boostrap

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const  { engine } =  require('express-handlebars');
+
+
+const app = express()
+
+const port = 3001
+
+app.use(morgan('combined'))
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log('test patch',path.join(__dirname, 'public/img'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(port, () => 
+  console.log(`Example app listening on port ${port}`)
+)
